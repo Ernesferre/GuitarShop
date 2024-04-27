113ce738297765f9ba3b814b55b31510ced27686
Ernesto.Ferreyra.-ND <Ernesto.Ferreyra.-ND@disney.com>
2024-04-27 18:31:57 -0300

    Directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from "../config/fonts";
+
 
 
 export default function Home() {
   return (
     <>
-      <h1>Home Ernest</h1>
+      <h1 className={`${titleFont.className} font-bold`}>Home</h1>
       <p>Welcome to the home page</p>
     </>
   );
