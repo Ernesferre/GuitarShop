a6ca01b8decc1ec2bdd182466b98a3f5eb6c2ee4
Ernesto.Ferreyra.-ND <Ernesto.Ferreyra.-ND@disney.com>
2024-04-27 20:22:18 -0300

    completing directory structure

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+  return (
+    <div>
+      <h1>Order #abc</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "../../../config/fonts";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={`${titleFont.className} font-bold`}>Login Layout</h1>
+    </div>
+  );
+}
